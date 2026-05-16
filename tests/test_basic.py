3909c942696ef50b2d4c094f4c0b7f0249a8f183
Rightup <lloyd@rightup.co.uk>
2026-05-16 11:54:57 +0100

    bump version to 1.0.11

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,7 @@ from pymc_core import CryptoUtils, LocalIdentity, MeshNode, Packet, __version__
 
 
 def test_version():
-    assert __version__ == "1.0.10"
+    assert __version__ == "1.0.11"
 
 
 def test_import():
